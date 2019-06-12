bcda328354029a3733c6045d27e48cd05c978809
Rohit Aggarwal <737rohit.a@gmail.com>
2019-06-12 12:39:44 +0530

    change in split

--- splitting.py
+++ splitting.py
@@ -1,3 +1,6 @@
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+import nltk
 
 f = open("out_text.txt", "r")
 text = f.read().split(".\n\n")
@@ -5,4 +8,11 @@ arr = []
 for i in text:
 	arr.append(i)
 	
-print(arr[127])
+arr2 = []
+for j in arr:
+	if 'Director' in j or 'Directors' in j:
+		arr2.append(j)
+
+stop = stopwords.words("english")
+
+
