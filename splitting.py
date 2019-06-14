bb0a6a3d536bd67a50c05d55417150b202fd9e52
Rohit Aggarwal <737rohit.a@gmail.com>
2019-06-15 01:36:23 +0530

    splitting done, refactoring remaining

--- splitting.py
+++ splitting.py
@@ -1,18 +1,28 @@
-from nltk.corpus import stopwords
-from nltk import word_tokenize
-import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+import numpy as np
 
 f = open("out_text.txt", "r")
 text = f.read().split(".\n\n")
 arr = []
 for i in text:
 	arr.append(i)
-	
+
+
 arr2 = []
+a = 1
 for j in arr:
 	if 'Director' in j or 'Directors' in j:
 		arr2.append(j)
+		
+		#np.concatenate(arr3,word_tokenize(j))
+vectorizer = TfidfVectorizer(use_idf=True, lowercase=True,stop_words='english')		
+X = vectorizer.fit_transform(arr2)	
+kmeans = KMeans(n_clusters=2,init='k-means++',max_iter=100).fit(X)
 
-stop = stopwords.words("english")
+arr3 = kmeans.labels_
+print(arr2[0])
+
+
 
 
