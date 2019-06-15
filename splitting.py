f6185f0a7b841455ce2c5996cd7a81831f910f19
Rohit Aggarwal <737rohit.a@gmail.com>
2019-06-15 12:49:33 +0530

    updated and maybe final function

--- splitting.py
+++ splitting.py
@@ -2,26 +2,40 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 import numpy as np
 
-f = open("out_text.txt", "r")
-text = f.read().split(".\n\n")
-arr = []
-for i in text:
-	arr.append(i)
+def powerDir(txtfile):
+	f = open(txtfile, "r")
+	text = f.read().split(".\n\n")
+	arr = []
+	for i in text:
+		arr.append(i)
 
+	arr2 = []
+	a = 1
+	for j in arr:
+		if 'Director' in j:
+			arr2.append(j)
 
-arr2 = []
-a = 1
-for j in arr:
-	if 'Director' in j or 'Directors' in j:
-		arr2.append(j)
+	vectorizer = TfidfVectorizer(use_idf=True, lowercase=True,stop_words='english')		
+	X = vectorizer.fit_transform(arr2)	
+	kmeans = KMeans(n_clusters=2,init='k-means++',max_iter=100).fit(X)
+
+	arr3 = kmeans.labels_
+	outfile = "power.txt"
+	outfile2 = "notpower.txt"
+	file = open(outfile,"a")
+	file2 = open(outfile2,"a")
+	for i,val in enumerate(arr3):
+		if val == 1:
+			file.write(arr2[i])
+		else:
+			file2.write(arr2[i])
+
+	file.close()
+	file2.close()
 		
-		#np.concatenate(arr3,word_tokenize(j))
-vectorizer = TfidfVectorizer(use_idf=True, lowercase=True,stop_words='english')		
-X = vectorizer.fit_transform(arr2)	
-kmeans = KMeans(n_clusters=2,init='k-means++',max_iter=100).fit(X)
 
-arr3 = kmeans.labels_
-print(arr2[0])
+		
+
 
 
 
